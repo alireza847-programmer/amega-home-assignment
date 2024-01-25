b0636b46255810154bcb38c8d414b9db9609df50
a.sheykholeslami <a.sheykholeslami@kian.digital>
2024-01-25 15:05:46 +0330

    ADD alias module and config it

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          root: ['./src/'],
+          pages: './src/pages',
+          components: './src/components',
+          themes: './src/themes',
+          utils: './src/utils',
+          routes: './src/routes',
+          types: './src/types',
+          assets: './src/assets',
+          layouts: './src/layouts',
+          stories: './src/stories',
+          locales: './src/locales',
+          hooks: './src/hooks',
+          stores: './src/stores',
+        },
+      },
+    ],
+  ],
 };
