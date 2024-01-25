8d83eaa1fabb03429c026c220d5bf7f224aa6b2f
a.sheykholeslami <a.sheykholeslami@kian.digital>
2024-01-25 17:15:30 +0330

    ADD export icons

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,6 @@
+import ChevronSvg from 'assets/svgs/chevron.svg';
+import CloseSvg from 'assets/svgs/close.svg';
+import PlusSvg from 'assets/svgs/plus.svg';
+import SearchSvg from 'assets/svgs/search.svg';
+
+export {ChevronSvg, CloseSvg, PlusSvg, SearchSvg};
